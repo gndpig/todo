805c1e8fff63dd957b11bf16d601110ef054b955
Taiki Nakatsuka <gnd.scandal.pig@gmail.com>
2013-03-03 22:51:07 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todo::Application.config.secret_key_base = 'e6f8515861bbd9c1ae786b756e0b1e38550321370bf255f7ec953ba57d1fb40c8cf35a9a438a25e36fa1e420caba81d0baba37447a71d7d7828664d218c7e660'
