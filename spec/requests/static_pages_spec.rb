f7cc55ac067d37367f6d02720ccb8af26081853a
Taiki Nakatsuka <gnd.scandal.pig@gmail.com>
2013-03-05 00:14:16 +0900

    add user page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
+
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_selector('h1', text: 'TODO List') }
+    it { should have_selector('title', text: 'TODO List') }
+  end
 end
